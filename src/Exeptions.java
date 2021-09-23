c8ca4168bc1a803bd9cd7fcdd07d0e0a907182a1
amfitaminka <33510266+amfitaminka@users.noreply.github.com>
2021-09-23 13:39:21 +0300

    Update Exeptions.java

--- src/Exeptions.java
+++ src/Exeptions.java
@@ -1,7 +1,39 @@
+
 public class Exeptions {
     public void exeption1(String a) {
-        if ((a.contains("1") || a.contains("2") || a.contains("3") || a.contains("4") || a.contains("5") || a.contains("6") || a.contains("7") || a.contains("8") || a.contains("9")) && (a.contains("X") || a.contains("I") || a.contains("II") || a.contains("III") || a.contains("IV") || a.contains("V") || a.contains("VI") || a.contains("VII") || a.contains("VIII") || a.contains("IX"))) {
+        if ((a.contains("1") || a.contains("2") || a.contains("3") || a.contains("4") || a.contains("5") || a.contains("6") || a.contains("7") || a.contains("8") || a.contains("9")) && ((a.contains("X") || a.contains("I") || a.contains("V")))){
             System.out.println("throws Exeptions");
+          System.exit(0);
         }
     }
+  	
+  	public String exeption2(String a, int d){
+      
+        if(d == 10){
+		a = "X";
+        }else if(d == 1){
+		a = "I";
+		}else if(d == 2){
+		a = "II";
+        }else if(d == 3){
+		a = "III";
+        }else if(d == 4){
+		a = "IV";
+        }else if(d == 5){
+		a = "V";
+        }else if(d == 6){
+		a = "VI";
+        }else if(d == 7){
+		a = "VII";
+        }else if(d == 8){
+		a = "VIII";
+        }else if(d == 9){
+		a = "IX";
+        }else{
+          a="throws Exeptions";
+        }
+
+    return a;
+    }
 }
+
