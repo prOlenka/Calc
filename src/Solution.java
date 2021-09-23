68342eb5bbb96b8fba18dae5a9f892100db03ea6
amfitaminka <33510266+amfitaminka@users.noreply.github.com>
2021-09-23 13:40:01 +0300

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,33 @@
 class Solution {
-    public int Calc(int i, String[] arr) {
+ public int Calc(int i, String[] arr) {
         int a = 0;
 
-        if(arr[i].contains("10")||arr[i].contains("X")){
+        if(arr[i].equals("10")||arr[i].equals("X")){
             a = 10;
-        }else if(arr[i].contains("1")||arr[i].contains("I")){
+        }else if(arr[i].equals("1")||arr[i].equals("I")){
                     a = 1;
-                }else if(arr[i].contains("2")||arr[i].contains("II")){
+                }else if(arr[i].equals("2")||arr[i].equals("II")){
                     a = 2;
-                }else if(arr[i].contains("3")||arr[i].contains("III")){
+                }else if(arr[i].equals("3")||arr[i].equals("III")){
                     a = 3;
-                }else if(arr[i].contains("4")||arr[i].contains("IV")){
+                }else if(arr[i].equals("4")||arr[i].equals("IV")){
                     a = 4;
-                }else if(arr[i].contains("5")||arr[i].contains("V")){
+                }else if(arr[i].equals("5")||arr[i].equals("V")){
                     a = 5;
-                }else if(arr[i].contains("6")||arr[i].contains("VI")){
+                }else if(arr[i].equals("6")||arr[i].equals("VI")){
                     a = 6;
-                }else if(arr[i].contains("7")||arr[i].contains("VII")){
+                }else if(arr[i].equals("7")||arr[i].equals("VII")){
                     a = 7;
-                }else if(arr[i].contains("8")||arr[i].contains("VIII")){
+                }else if(arr[i].equals("8")||arr[i].equals("VIII")){
                     a = 8;
-                }else if(arr[i].contains("9")||arr[i].contains("IX")){
+                }else if(arr[i].equals("9")||arr[i].equals("IX")){
                     a = 9;
                 }
 
         return a;
     }
 
-    public int rezultat(String a, int b, int c){
+    public int count(String a, int b, int c){
         int rez = 0;
         if(a.contains("+")){
             rez = b+c;
@@ -40,7 +40,5 @@ class Solution {
         }
         return rez;
     }
-
-
 
 }
